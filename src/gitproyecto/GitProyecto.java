8cd2745283ee86745a856f1b7267ed478b5e2dd7
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-16 15:41:48 -0500

    Impresion en la rama testeo

--- src/gitproyecto/GitProyecto.java
+++ src/gitproyecto/GitProyecto.java
@@ -7,6 +7,7 @@ public class GitProyecto {
         System.out.println("Otra rama nueva, usando un doble comando.");
         System.out.println("Se supone que esto es lo primero que voy a "
                 + "subir");
+        System.out.println("Cambios desde la rama testeo");
     }
 
 }
