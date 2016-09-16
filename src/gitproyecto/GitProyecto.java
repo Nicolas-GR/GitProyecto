3ef90046ec78e69a36b632caabd52f7a0b1d0a88
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-16 12:29:52 -0500

    Línea de impresión nueva como algo nuevo

--- src/gitproyecto/GitProyecto.java
+++ src/gitproyecto/GitProyecto.java
@@ -4,6 +4,7 @@ public class GitProyecto {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo, esto es Git");
+        System.out.println("Otra rama nueva, usando un dobre comando.");
     }
 
 }
