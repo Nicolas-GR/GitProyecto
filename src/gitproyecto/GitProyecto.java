b40c0f50a15da92843c5e526d31c91df5069c0ca
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-16 15:37:29 -0500

    Línea para ver en el repositorio remoto

--- src/gitproyecto/GitProyecto.java
+++ src/gitproyecto/GitProyecto.java
@@ -5,6 +5,8 @@ public class GitProyecto {
     public static void main(String[] args) {
         System.out.println("Hola mundo, esto es Git");
         System.out.println("Otra rama nueva, usando un doble comando.");
+        System.out.println("Se supone que esto es lo primero que voy a "
+                + "subir");
     }
 
 }
